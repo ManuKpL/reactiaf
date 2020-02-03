88f84a8bf51ebe23760132583d57b9729b35cbc7
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2020-02-03 08:31:44 +0100

    fix(Loader) added missing proptypes & default props

--- src/shared/Loader/Loader.js
+++ src/shared/Loader/Loader.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import './Loader.scss';
 
 export const Loader = ({ center }) => <span className={classnames('loader', { centered: center })}></span>;
+
+Loader.propTypes = {
+  center: PropTypes.bool,
+};
+
+Loader.defaultProps = {
+  center: false,
+};
